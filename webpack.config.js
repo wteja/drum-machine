60cb3df3b3dcdb53a1942049a401e2f209aac953
Weerayut Teja <teja.weerayut@gmail.com>
2020-01-07 16:57:55 +0700

    Adding favicon

--- webpack.config.js
+++ webpack.config.js
@@ -5,60 +5,65 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const distDir = path.resolve(__dirname, 'dist');
 
-let config = {
-    mode,
-    entry: [
-        'react-hot-loader/patch',
-        './src/index.js'
-    ],
-    output: {
-        filename: 'bundle.js',
-        path: distDir
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"],
-                        plugins: ["react-hot-loader/babel"]
-                    }
-                }
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src/")
+let exportConfig = ({ mode }) => {
+    const config = {
+        mode,
+        entry: [
+            'react-hot-loader/patch',
+            './src/index.js'
+        ],
+        output: {
+            filename: 'bundle.js',
+            path: distDir
         },
-        extensions: ['*', '.js', '.jsx']
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
-    ]
-}
-
-if (mode === 'development') {
-    config.devtool = 'inline-source-map';
-    config.devServer = {
-        inline: true,
-        hot: true,
-        compress: false
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/i,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                            plugins: ["react-hot-loader/babel"]
+                        }
+                    }
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src/")
+            },
+            extensions: ['*', '.js', '.jsx']
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public', 'index.html'),
+                favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
+            })
+        ]
     }
+
+    if (mode === 'development') {
+        config.devtool = 'inline-source-map';
+        config.devServer = {
+            inline: true,
+            hot: true,
+            compress: false
+        }
+    }
+
+    return config;
 }
 
-module.exports = config;
+module.exports = exportConfig;
